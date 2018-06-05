5dd0a193fc47128c1fdac1d98da7abce8411ca96
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-06-05 18:43:21 -0400

    wiki model update:

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,12 +1,20 @@
 class Wiki < ApplicationRecord
-  belongs_to :user, required: false
+  belongs_to :user
   has_many :users, through: :collaborators
 
-  before_save { self.private == false }
+  before_save :set_to_false
 
   default_scope { order('created_at DESC') }
 
   validates :title, length: { minimum: 2 }, presence: true
   validates :body, length: { minimum: 5 }, presence: true
   validates :user, presence: true
+
+  private
+
+  def set_to_false
+    if @wiki.private.nil
+      self.private == false
+    end
+  end
 end
