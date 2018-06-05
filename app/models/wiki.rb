b5676876be320a8ce2ad0ac197ec6e5d3c97241a
Drwhirstyburgers <dave.hirst50@gmail.com>
2018-06-05 17:31:21 -0400

    minor change to wiki model

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,6 +1,6 @@
 class Wiki < ApplicationRecord
   belongs_to :user, required: false
-  has_many :collaborators
+  has_many :users, through: :collaborators
 
   before_save { self.private == false }
 
